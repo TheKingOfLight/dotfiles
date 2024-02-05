16c31cbbe3e8c630a498e6b8d7da2d9baeb6f0c4
TheKingOfLight <king-of-the-light@gmx.de>
2024-02-05 21:51:23 +0100

    display-setting.py script handeling the monitors

--- .config/wofi/scripts/display-settings.sh
+++ .config/wofi/scripts/display-settings.sh
@@ -6,7 +6,7 @@ selected=$(echo -e $entries|wofi --style $HOME/.config/wofi/Mint-Y-Aqua.css --dm
 
 case $selected in
   default)
-   exec swaymsg reload;;
+        exec sh .local/bin/display-setting.sh default;
   gaming)
-    exec swaymsg output DP-1 background $HOME/.local/share/backgrounds/hells-paradise--yamada-fire-with-sword.jpg fill, output DP-1 mode 1920x1080@144Hz, output DP-1 position 1920 0, output DP-1 adaptive_sync on;;
+        exec sh .local/bin/display-setting.sh gaming;
 esac
